ac9db9d335a49f224a01ffb86ede87f6a4f89fe6
Lenovo <Lenovo@AIO>
2022-12-25 13:50:07 +0100

    First Commit

---
+++ src/application/SampleController.java
@@ -0,0 +1,25 @@
+package application;
+
+import java.awt.event.ActionEvent;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class SampleController {
+	@FXML
+    private Button login;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private TextField usernameField;
+    
+    @FXML
+     void onLogin(ActionEvent event) {
+
+    }
+	
+}
