b05adf42e3b214e25f35c37a814b6829270274ba
DELL <DELL@DESKTOP-UTDQUDB>
2023-01-02 14:26:07 +0100

    adda abdelilah : create interface and controller

---
+++ src/Controllers/clientController.java
@@ -0,0 +1,12 @@
+package Controllers;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+public class clientController extends Application {
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		
+	}
+
+}
---
+++ src/Controllers/utilisateurController.java
@@ -0,0 +1,13 @@
+package Controllers;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class utilisateurController extends Application {
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		
+	}
+
+}
