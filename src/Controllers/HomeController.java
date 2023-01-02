187df753abd74e55820df224dc0bf6e39938eb73
Malek Ad <Malek Ad@malek>
2023-01-02 16:45:42 +0100

    liée les interfaces

--- src/Controllers/HomeController.java
+++ src/Controllers/HomeController.java
@@ -30,32 +30,68 @@ public class HomeController implements Initializable {
 
     @FXML
     void btn_Client(MouseEvent event) {
-    	
+    	try {
+			fxml=FXMLLoader.load(getClass().getResource("/Interfaces/client.fxml"));
+			root.getChildren().removeAll();
+			root.getChildren().setAll(fxml);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
     }
 
     @FXML
     void btn_Home(MouseEvent event) {
-    	
+    	try {
+			fxml=FXMLLoader.load(getClass().getResource("/Interfaces/Home.fxml"));
+			root.getChildren().removeAll();
+			root.getChildren().setAll(fxml);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
     }
 
     @FXML
     void btn_Medicament(MouseEvent event) {
-    	
+    	try {
+			fxml=FXMLLoader.load(getClass().getResource("/Interfaces/Medicament.fxml"));
+			root.getChildren().removeAll();
+			root.getChildren().setAll(fxml);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
     }
 
     @FXML
     void btn_Statistique(MouseEvent event) {
-    	
+    	try {
+			fxml=FXMLLoader.load(getClass().getResource("/Interfaces/statistic.fxml"));
+			root.getChildren().removeAll();
+			root.getChildren().setAll(fxml);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}	
     }
 
     @FXML
     void btn_Vente(MouseEvent event) {
-    	
+    	try {
+			fxml=FXMLLoader.load(getClass().getResource("/Interfaces/Ventepage.fxml"));
+			root.getChildren().removeAll();
+			root.getChildren().setAll(fxml);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
     }
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		
+		try {
+			fxml=FXMLLoader.load(getClass().getResource("/Interfaces/Home.fxml"));
+			root.getChildren().removeAll();
+			root.getChildren().setAll(fxml);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 	}
 
